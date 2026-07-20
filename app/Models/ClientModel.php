be019cb84bdb72eedcca0d5d25902c53eb65e789
no way <tflowtwitler@gmail.com>
2026-07-20 10:36:15 +0300

    donnes de test

--- app/Models/ClientModel.php
+++ app/Models/ClientModel.php
@@ -5,7 +5,7 @@ use CodeIgniter\Model;
 class ClientModel extends Model
 {
     protected $table = 'clients';
-    protected $allowedFields = ['nom_client','numero'];
+    protected $allowedFields = ['nom_client','numero','solde'];
 
     public function getClientByNumero(string $numero)
     {
